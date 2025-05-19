c7b01892c4f30d4717fe68ad36d955015e3282e0
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-05-19 13:44:25 +0300

    A tax consultancy company

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 const Map: React.FC = () => {
   // Coordinates for Mombasa city center
   const position: [number, number] = [-4.0435, 39.6682];
-  return <MapContainer center={position} zoom={13} className="h-96 w-full rounded-lg shadow-lg">
+  return <MapContainer center={position} zoom={13} scrollWheelZoom={false} className="h-96 w-full rounded-lg shadow-lg">
       <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={position}>
         <Popup>
